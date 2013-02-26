bacd036fc97446f5d4b6b6a8e78d460cd03f4d9a
Mahesh Asolkar <mahesh@mahesha.com>
2013-02-26 12:24:21 -0800

    Look, timezone changes:
    * Added Pacific timezone (need to figure a way to make it per user)
    * Showing log entry timestamp on hover
    * Showing smaller delete button on hover

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,10 @@ require File.expand_path('../application', __FILE__)
 #
 # Time formats
 #
-Time::DATE_FORMATS[:log_time_dy] = "%d"
-Time::DATE_FORMATS[:log_time_mn] = "%b"
-Time::DATE_FORMATS[:log_time_yr] = "%Y"
+Time::DATE_FORMATS[:log_time_dy]  = "%d"
+Time::DATE_FORMATS[:log_time_mn]  = "%b"
+Time::DATE_FORMATS[:log_time_yr]  = "%Y"
+Time::DATE_FORMATS[:log_time]     = "%l:%M%P"
 
 # Initialize the rails application
 RorWorklog::Application.initialize!
