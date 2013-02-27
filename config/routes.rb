94a2227c4b4fde5f277d0841ddbf036345a3d833
Mahesh Asolkar <mahesh@mahesha.com>
2013-02-27 14:55:37 -0800

    Minor changes to static page contents

--- config/routes.rb
+++ config/routes.rb
@@ -2,26 +2,22 @@ RorWorklog::Application.routes.draw do
   #
   # Paths
   #
-  # root :to => 'static_pages#home'
-  root :to => 'users#show'
+  root :to => 'users#show', :constraints => lambda { |req| !req.session[:user_id].blank? }
+  root :to => 'static_pages#home', :constraints => lambda { |req| req.session[:user_id].blank? }
 
   match '/home',    to: 'static_pages#home'
   match '/help',    to: 'static_pages#help'
-  match '/about',   to: 'static_pages#about'
   match '/signup',  to: 'users#new'
   match '/login',   to: 'sessions#new'
   match '/logout',  to: 'sessions#destroy'
   match '/profile', to: 'users#show'
-
-  match '/users/:username/edit', to: 'users#edit'
-  match '/users/:username', to: 'users#update', :via => :put
-  match '/users/:username', to: redirect { |params, req| "/#{params[:username]}" }
 
   #
   # Paths for user scoped access
   #
   scope '/:username', :constraints => ProfileConstraint do
     get '' => 'users#show'
+    put '' => 'users#update'
     get 'edit' => 'users#edit'
     resources :logs do
       resources :entries
